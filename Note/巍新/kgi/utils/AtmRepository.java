f62b7e15e5fc6895ac5f02c31a04cf71435bef42
cc54188 <71747740+cc54188@users.noreply.github.com>
2022-03-23 09:39:21 +0800

    Delete Note/巍新/kgi directory

--- Note/巍新/kgi/utils/AtmRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kgi.utils;
-
-import com.example.kgi.models.Atm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AtmRepository extends JpaRepository<Atm, Long> {
-}
--- Note/巍新/kgi/utils/AtmService.java
+++
@@ -1,4 +0,0 @@
-package com.example.kgi.utils;
-
-public interface AtmService {
-}
