b7b31382de70d2bc652b7afb4640fc48ad789f96
cc54188 <71747740+cc54188@users.noreply.github.com>
2022-04-08 16:25:53 +0800

    Delete Note/巍新/短網址產生器/shortcut directory

--- Note/巍新/短網址產生器/shortcut/ShortcutRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.shortcut;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShortcutRepository extends JpaRepository<Shortcut, Long> {
-
-    public Shortcut getByShortUrl(String shortUrl);
-}
--- Note/巍新/短網址產生器/shortcut/ShortcutService.java
+++
@@ -1,8 +0,0 @@
-package com.example.shortcut;
-
-public interface ShortcutService {
-
-    public Shortcut createUrl(String longUrl);
-
-    public String link(String shortUrl);
-}
