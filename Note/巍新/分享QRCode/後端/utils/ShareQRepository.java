33199a9c942879b1ac3539095dcf7de896ffaadb
cc54188 <71747740+cc54188@users.noreply.github.com>
2022-04-15 22:57:32 +0800

    Delete Note/巍新/分享QRCode/後端 directory

--- Note/巍新/分享QRCode/後端/utils/ShareQRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shareq.utils;
-
-import com.example.shareq.models.ShareQ;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShareQRepository extends JpaRepository<ShareQ, Long> {
-
-    public ShareQ getByShortUrl(String shortUrl);
-}
